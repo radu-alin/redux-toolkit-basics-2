e2b8080c02d3042dfc3e9487040743fed9ed44a6
Alin RADU <cristian.alin.radu@reea.net>
2022-11-09 16:30:02 +0200

    react | redux/toolkit | typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { CakeView } from './components/Cake/Cake'
+import { IcecreamView } from './components/Icecream/Icecream'
+import { UserView } from './components/User/User'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CakeView />
+      <IcecreamView />
+      <UserView />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/User/User.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import {fetchUsers } from '../../features/user/userSlice';
+
+export const UserView = () => {
+  const user = useAppSelector(state => state.user)
+  const dispatch = useAppDispatch()
+  
+  useEffect(() => {
+    dispatch(fetchUsers())
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div>
+      <h2>List of Users</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  )
+}
